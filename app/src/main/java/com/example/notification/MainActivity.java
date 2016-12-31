3a3e95eb4c3ce342c2d1f5a98355115d4e936b21
king <573723831@qq.com>
2016-12-31 10:34:22 +0800

    succeeded

--- app/src/main/java/com/example/notification/MainActivity.java
+++ app/src/main/java/com/example/notification/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.notification;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button=(Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NotificationManager manager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+                Notification notification;
+                Intent intent=new Intent(MainActivity.this,MainActivity.class);
+                PendingIntent pi=PendingIntent.getActivity(MainActivity.this,0,intent,PendingIntent.FLAG_CANCEL_CURRENT);
+                Notification.Builder builder = new Notification.Builder(MainActivity.this).setTicker("hello").setSmallIcon(R.mipmap.ic_launcher);
+                notification = builder.setContentTitle("hello").setContentText("world").setContentIntent(pi).build();
+                manager.notify(1,notification);
+            }
+        });
     }
 }
